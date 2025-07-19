0b017f253a86ecb8049a5a80d6120245296ddc29
Pranay Saini <sainipranay36@gmail.com>
2025-07-19 10:50:58 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://pro-linker-two.vercel.app/',
   credentials: true
 }));
 app.use(express.json({limit: '10mb'}));
